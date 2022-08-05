572579cebeffc5983341c857a7a15d39f5f2148b
Marcel Nishihara <marcelnishihara@gmail.com>
2022-08-05 00:41:09 -0300

    Code pattern fixed

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from GitLab import GitLab
 gitlab = GitLab(token='YOUR_GITLAB_ACCESS_TOKEN_HERE')
 gitlab.get_subgroup_repositories()
 repo_info = gitlab.compose_repositories_info()
-gitlab.log(repo_info)
+gitlab.log(obj_to_log=repo_info)
 gitlab.download_repositories()
